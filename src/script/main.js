14f02603f4be9cc33a618d8fc2178805e450a356
ultranext <991390671@qq.com>
2016-09-07 23:47:01 +0800

    before widget

--- src/script/main.js
+++ src/script/main.js
@@ -1,23 +1,27 @@
 require.config({
 	paths:{
 		jquery:"../lib/jquery-1.12.2.min",
+		jqueryUI:"../lib/jquery-ui-1.12.0/jquery-ui"
 	}
 })
 
-require(['jquery','window'],function($,w){
+require(['jquery','window','jqueryUI'],function($,w,$UI){
 
 	$('#btn').on('click',function(){
-		new w.Window().alert({content:'welcome',
+		var win = new w.Window()
+		win.alert({content:'welcome',
 			width:300,
 			height:200,
 			y:100,
 			hasCloseBtn:true,
+			hasMask:true,
+			dragHandle:".title",
 			handler_cofm_btn: function(){
 				alert("you alert comfirm button");
 			},
 			handler_close_Btn: function(){
 				alert('you alert comfirm button');
 			}
-	})
+	});
 	})
 })
--- src/script/window.js
+++ src/script/window.js
@@ -1,7 +1,8 @@
-define(['jquery'],function($){
+define(['widget','jquery','jqueryUI'],function(widget,$,$UI){
 
 	function Window (){
-
+		
+		this.handlers ={};
 		this.cfg={
 			width:200,
 			height:150,
@@ -9,20 +10,36 @@ define(['jquery'],function($){
 			title:'系统提示',
 			hasCloseBtn:false,
 			handler_close_Btn:null,
-			handler_cofm_btn:null
+			handler_cofm_btn:null,
+			skinClass:null,
+			textForCofmBtn:"确定",
+			hasMask:false,
+			ifDraggable:true,
+			dragHandle:null,
+
 		}
 	};
-	Window.prototype = {
+	Window.prototype =$.extend({},new widget.Widget(),{
+		
 		alert:function(cfg){
+			var that = this;
+			var mask = null;
 			var CFG = $.extend(this.cfg,cfg);
 			var boundingBox = $('<div class="window_boundingBox"><header class="title">'+CFG.title+
 				'</header><div class="msg_zone">'+ CFG.content+
-				'</div><footer><input type="button" value="确定" class="cofm_btn"></footer><div/>');
+				'</div><footer><input type="button" value="'+CFG.textForCofmBtn+'" class="cofm_btn"></footer><div/>');
 			boundingBox.appendTo("body");
+			if(CFG.hasMask){
+				mask = $('<div class="mask"></div>');
+				mask.appendTo('body');
+			}
 			var btn = $('.cofm_btn');
 			btn.click(function(){
-				CFG.handler_cofm_btn&&CFG.handler_cofm_btn();
+				//CFG.handler_cofm_btn&&CFG.handler_cofm_btn();
+				that.fire('alert');
 				boundingBox.remove();
+				mask.remove();
+
 			})
 			
 			boundingBox.css({
@@ -35,14 +52,34 @@ define(['jquery'],function($){
 				var closeBtn = $('<img src="src/images/icon_close_alt2.png" alt="close" class="closeBtn"/>');
 				closeBtn.appendTo(boundingBox);
 				closeBtn.click(function(){
-					CFG.handler_close_Btn&&CFG.handler_close_Btn();
+					//CFG.handler_close_Btn&&CFG.handler_close_Btn();
+					that.fire('close');
 					boundingBox.remove();
+					mask.remove();
 				})
 			}
+			if(CFG.skinClass){
+				boundingBox.addClass(CFG.skinClass);
+				
+			}
+			if(CFG.ifDraggable){
+				if(CFG.dragHandle){
+					boundingBox.draggable({handle:CFG.dragHandle});
+				}
+				boundingBox.draggable();
+			}
+			if(CFG.handler_cofm_btn){
+				this.on('alert',CFG.handler_cofm_btn);
+			}
+			if(CFG.handler_close_Btn){
+				this.on('close',CFG.handler_close_Btn);
+			}
+
+			return this;
 		},
 		confirm:function(){},
 		prompt: function(){}
-	}
+	}) 
 	return{
 		Window:Window
 	}
